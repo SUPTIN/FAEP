7048456edfff1aa2f29d4af6d984f43f61f9b5f2
elyrio <lyrio.nas@mail.com>
2018-05-02 12:41:20 -0300

    Criando formulario de edicao.

--- app/Http/Controllers/PeriodoController.php
+++ app/Http/Controllers/PeriodoController.php
@@ -10,33 +10,45 @@ use App\periodo;
 
 class PeriodoController extends Controller
 {
-	private $totalPage = 20;
+	private $totalPage = 10;
 	private $periodo;
 
 	public function __construct(periodo $periodo){
     	$this->periodo = $periodo;
     }
 
-
-    public function  viewListaPeriodos(){ 
-    	$periodos = $this->periodo->orderBy('id')->paginate($this->totalPage);
+    public function viewListaPeriodos(){ 
+    	$periodos = $this->periodo->orderBy('id','desc')->paginate($this->totalPage);
     	return view('periodosLista', compact('periodos'));
     }
 
-    public function  viewFormPeriodos(){ 
+    public function viewFormPeriodos(){ 
         return view('periodosForm');
     }
 
-    public function  addNovoPeriodoAva(Request $request){ 
-
+    public function addNovoPeriodoAva(Request $request){ 
         $dados = $request->except('_token');
         if ($request->ativo != '1'){
             $dados["ativo"] = '0';
         }
         $this->validate($request, $this->periodo->rules, $this->periodo->messages);
-
         $this->periodo->create($dados);
+        return redirect('periodos');
+    }
 
-        return PeriodoController::viewListaPeriodos();
+    public function viewFormEditPeriodo(Request $request){ 
+        $id= $request->id;
+        $dados = periodo::find($id);
+        return view('periodosFormEdit', array('dados' => $dados));
+    }
+
+    public function atualizaPeriodoAva(Request $request){ 
+        $id= $request->id;
+        $dados = periodo::find($id);
+        if ($dados && $dados->exists){
+            $parametros = $request->all();
+            $dados->fill($parametros)->save();
+        }
+        return redirect('periodos');
     }
 }
