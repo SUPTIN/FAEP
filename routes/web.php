780baff9de264548d3a4ef44d9d8337f0b973d5d
elyrio <lyrio.nas@mail.com>
2018-05-03 13:53:36 -0300

    Criando rotas para local de trabalho.

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,32 @@
 Route::get('informacao', 'FaepController@informacao');
 Route::get('logs', 'FaepController@viewLog');
 
+
 Route::get('aceite', 'AvaliarController@viewAceite');
 Route::get('listaAvaliados', 'AvaliarController@viewListaAvaliar');
 
+
 Route::get('listaAvaliadores', 'AvaliadorController@viewListaAvaliadores');
+
+
+Route::get('secretarias', 'SecretariaController@viewListaSec');
+Route::get('novaSecretaria', 'SecretariaController@viewFormSecretaria');
+Route::post('addSecretaria', 'SecretariaController@addNovaSecretaria');
+Route::get('{id}/viewSecretaria', 'SecretariaController@viewSecretaria');
+Route::get('{id}/editSecretaria', 'SecretariaController@viewFormEditSecretaria');
+Route::post('{id}/updateSecretaria', 'SecretariaController@atualizaSecretaria');
+
+Route::get('{id}/novoLocalTrab', 'SecretariaController@viewFormLocalTrab');
+Route::post('{id}/addLocalTrab', 'SecretariaController@addNovoLocalTrab');
+Route::get('{id}/editLocalTrab', 'SecretariaController@editFormLocalTrab');
+Route::post('{id}/updateLocalTrab', 'SecretariaController@atualizaLocalTrab');
+
 
 Route::get('periodos', 'PeriodoController@viewListaPeriodos');
 Route::get('novoPeriodo', 'PeriodoController@viewFormPeriodos');
 Route::post('addPeriodo', 'PeriodoController@addNovoPeriodoAva');
+Route::get('{id}/editPeriodo', 'PeriodoController@viewFormEditPeriodo');
+Route::post('{id}/updatePeriodo', 'PeriodoController@atualizaPeriodoAva');
 
 
 Route::get('/', function () {
